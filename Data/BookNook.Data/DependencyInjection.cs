7f25f9cd89f1f9db1e36a0706808a738803beffc
Viktor <stu2401322041@uni-plovdiv.bg>
2026-09-22 17:33:50 +0300

    Add connection string, set up DI and register the DbContext in startup

---
+++ Data/BookNook.Data/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BookNook.Data
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddDataLayer(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<BookNookContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            return services;
+        }
+    }
+}
--- Web/BookNook.Web/Program.cs
+++ Web/BookNook.Web/Program.cs
@@ -1,6 +1,11 @@
+using BookNook.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDataLayer(builder.Configuration.GetConnectionString("DefaultConnection")!);
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
